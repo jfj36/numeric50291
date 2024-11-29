1fbbdd42b805bdc4f007a963f6292721500de32a
jfj36 <jfj36@bath.ac.uk>
2024-11-29 12:46:42 +0000

    Modification of the __init__.py file to create a package

--- simple_package/__init__.py
+++ simple_package/__init__.py
@@ -1,1 +1,5 @@
 # Init file for the package
+# You can only import modules, not packages. Packages are just containers for modules or sub-packages.
+# When you "import" a package you actually import the module __init__.py.
+
+from simple_package import operations, statistics, graphics
