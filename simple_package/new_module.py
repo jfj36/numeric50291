8b65f759b22afcc6b6d693ec658115dcc333bdd0
jfj36 <jfj36@bath.ac.uk>
2024-12-01 11:26:20 +0000

    A description of the function split in the file has been added

--- simple_package/new_module.py
+++ simple_package/new_module.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 def split_data(X, y, test_size=0.2, random_state=42, stratify=True):
-    # Split the data into training and test sets with stratification
+    """# Split the data into training and test sets with stratification"""
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state, stratify=y)
     
     return X_train, X_test, y_train, y_test
